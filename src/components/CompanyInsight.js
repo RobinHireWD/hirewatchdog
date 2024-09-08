4272dab5f01018c6e4545b547ce30bfc0feedb6d
Robin <singh.robin.r84@gmail.com>
2024-09-08 21:42:13 +0200

    imporant - sync beteen both pages working

--- src/components/CompanyInsight.js
+++ src/components/CompanyInsight.js
@@ -38,7 +38,7 @@ const CompanyInsight = () => {
               <h2>{company.name || 'N/A'}</h2>
               <p>{company.country || 'Unknown Country'}</p>
               <div className="rating-circle">
-                <p>{(company.rating !== undefined ? company.rating.toFixed(1) : 'N/A')}</p>
+                <p>{company.rating !== null && company.rating !== undefined ? company.rating.toFixed(1) : 'N/A'}</p>
                 <span>Overall Rating</span>
               </div>
             </div>
@@ -46,19 +46,19 @@ const CompanyInsight = () => {
               <div className="hiring-info">
                 <div className="info-item">
                   <p>Average Job Online Time (Weeks)</p>
-                  <p className="green">{(company.avgOnlineTime !== undefined ? company.avgOnlineTime.toFixed(1) : 'N/A')}</p>
+                  <p className="green">{company.avgOnlineTime !== null && company.avgOnlineTime !== undefined ? company.avgOnlineTime.toFixed(1) : 'N/A'}</p>
                 </div>
                 <div className="info-item">
                   <p>Average Feedback Time (Weeks)</p>
-                  <p className="yellow">{(company.avgfeedbacktime !== undefined ? company.avgfeedbacktime.toFixed(1) : 'N/A')}</p>
+                  <p className="yellow">{company.avgfeedbacktime !== null && company.avgfeedbacktime !== undefined ? company.avgfeedbacktime.toFixed(1) : 'N/A'}</p>
                 </div>
                 <div className="info-item">
                   <p>Number of Feedbacks</p>
-                  <p className="blue">{(company.num_feedback !== undefined ? company.num_feedback : 'N/A')}</p>
+                  <p className="blue">{company.num_feedback !== null && company.num_feedback !== undefined ? company.num_feedback : 'N/A'}</p>
                 </div>
                 <div className="info-item">
                   <p>Ghost Job Probability</p>
-                  <p className="red">{(company.ghostjobprobability !== undefined ? (company.ghostjobprobability * 100).toFixed(1) : 'N/A')}%</p>
+                  <p className="red">{company.ghostjobprobability !== null && company.ghostjobprobability !== undefined ? (company.ghostjobprobability * 100).toFixed(1) : 'N/A'}%</p>
                 </div>
               </div>
               <div className="application-info">
